35ee2fc32f3fd63b3ce274a87896c44ca4595d8b
Maria Eduarda dos Santos <mariaes3@al.insper.edu.br>
2025-03-25 16:37:58 -0300

    teste1

--- funcoes.py
+++ funcoes.py
@@ -1,11 +1,16 @@
-import pygame
+import pygame  
+from random import randint
+
 def inicializa():
     pygame.init()
 
     window = pygame.display.set_mode((320, 240))
     pygame.display.set_caption("Jogo da Maria Eduarda dos Santos")
-
-    return window
+    assets = {"nave": pygame.image.load("assets/img/playerShip1_orange.png"), 
+              "fundo": pygame.image.load("assets/img/starfield.png")
+                }
+    
+    return window, assets
 
 def recebe_eventos():
 
@@ -16,12 +21,16 @@ def recebe_eventos():
 
     return game
 
-def desenha (window):
+def desenha (window, assets):
     pygame.display.update()
     tela = window.fill((255, 0, 0))
+    window.blit(assets["nave"], (160,240))  
+    window.blit(assets["fundo"], (0,0))  
+    pygame.draw.circle(window, (255,255,255), (randint(0,320), randint(0,240)), 2)
+
     return tela
     
-def game_loop(window):
+def game_loop(window, assets):
 
     game = True
 
@@ -31,10 +40,8 @@ def game_loop(window):
         game = recebe_eventos()
         if game:
             # ----- Gera saídas
-            desenha(window)
+            desenha(window, assets)
 
-
-
-
-
-
+if __name__ == '__main__':
+    window, assets= inicializa()
+    game_loop(window, assets)
